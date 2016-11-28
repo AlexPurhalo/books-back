397dc20b8555b5b52be9f952114f78b9167f667c
AlexPurhalo <alexpurhalo@gmail.com>
2016-11-28 21:35:06 +0200

    rendering-books-by-genre

--- app/api/books.rb
+++ app/api/books.rb
@@ -1,7 +1,14 @@
 class Books < Grape::API
   resources :books do
     get '/', rabl: 'books/index' do
-      @books = Book.all
+      params.each do |param, param_genre|
+        if param == 'genre'
+          genre_in_db = Genre.where(genre: param_genre).first
+          genre_in_db && (@sorted_books = Book.where(genre_id: genre_in_db.id))
+        end
+      end
+
+      @sorted_books ? (@books = @sorted_books) : (@books = Book.all)
     end
 
     get '/:id', rabl: 'books/show' do
